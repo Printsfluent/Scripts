4fa752fbb7e8594ac0d50281fa9c0367509adeae
Damilare Lawal <Rasco2703@gmail.com>
2023-02-19 23:29:40 -0800

    File Creation Update

--- file_create.sh
+++ file_create.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
-echo "Enter the name of your file: "
-read file_name
+echo "Enter File Name "
+read filename
 
-touch $file_name
+touch $filename
 
-read -p "This is an empty file. Would you like to write to it? Enter y or n:  " input
+read -p "This is an empty file. Would you like to write to it? Enter a or b:  " input
 
-if [ "$input" = "y" ]; then
-	read -p "enter vi to use vi, enter emacs to use emacs" editor
+if [ "$input" = "a" ]; then
+	read -p "enter vi to use vi   " editor
 
 	if [ "$editor" = "vi" ]; then
-		vi $file_name
-		echo "$file_name saved successfully"
-	elif [ "$editor" = "emacs" ]; then
-		emacs $file_name
-		echo "$file_name saved successfully"
+		vi $filename
+		echo "$filename saved successfully"
 	else
 		echo "Wrong input. Your file has been created"
 	fi
 
 elif [ "$input" = "n" ]; then
-	echo "$file_name saved successfully!"
+	echo "$filename saved successfully!"
 
 else
 	echo "Wrong input"
 
 fi
 
-chmod 777 $file_name
+chmod 777 $filename
 
